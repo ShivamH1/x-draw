608bc9b3babd40d28f49cdca24135e2a2f709eb4
ShivamH1 <shivamhonra@gmail.com>
2025-08-12 22:20:47 +0530

    turborepo

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Transpile local workspace packages that ship uncompiled TS/TSX
+  transpilePackages: ["@repo/ui"],
+};
 
 export default nextConfig;
