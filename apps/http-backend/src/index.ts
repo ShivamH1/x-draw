98546a14729e2ac204f4cef45e7e03ed243bc6e5
ShivamH1 <shivamhonra@gmail.com>
2025-08-19 18:04:01 +0530

    add common package and zod schema

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,32 +1,44 @@
 import express, { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { middleware } from "./middleware";
+import { JWT_SECRET } from "@repo/backend-common/config";
+import {
+  createUserSchema,
+  signInSchema,
+  createRoomSchema,
+} from "@repo/common/types";
 
 const app = express();
 
 app.post("/signup", async (req: Request, res: Response) => {
-  const { email, password } = req.body;
+  const data = createUserSchema.safeParse(req.body);
+  if (!data.success) {
+    return res.json({
+      message: "Incorrect Inputs",
+    });
+  }
+  const { username, password } = req.body;
   // const user = await User.findOne({ email });
   const userId = 1;
   if (userId) {
     return res.status(400).json({ message: "User already exists" });
   }
-  // const newUser = new User({ email, password });
+  // const newUser = new User({ username, password });
   // await newUser.save();
   res.status(201).json({ message: "User created successfully" });
 });
 
 app.post("/signin", async (req: Request, res: Response) => {
-  const { email, password } = req.body;
+  const { username, password } = req.body;
   // const user = await User.findOne({ email });
-  const user = {password}
+  const user = { password };
   if (!user) {
     return res.status(400).json({ message: "User not found" });
   }
   if (user.password !== password) {
     return res.status(400).json({ message: "Invalid password" });
   }
-  const token = jwt.sign({ email }, process.env.JWT_SECRET as string, {
+  const token = jwt.sign({ username }, JWT_SECRET as string, {
     expiresIn: "12h",
   });
   res.status(200).json({ message: "User signed in successfully", token });
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,5 +1,6 @@
 import { WebSocketServer } from "ws";
 import jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/backend-common/config";
 
 const wss = new WebSocketServer({ port: 8081 });
 
@@ -12,10 +13,7 @@ wss.on("connection", (ws, request) => {
     }
     const queryParams = new URLSearchParams(url.split("?")[1]);
     const token = queryParams.get("token") || "";
-    const decoded = jwt.verify(
-      token,
-      process.env.JWT_SECRET as string
-    ) as JwtPayload;
+    const decoded = jwt.verify(token, JWT_SECRET as string) as JwtPayload;
 
     if (!decoded || !decoded.userId) {
       ws.close();
